fc9d8ad88ebf6a4d5215b511f6d638c0c949dcf6
Miguel Oliveira <miguel.gazela@gmail.com>
2013-03-05 20:51:26 +0000

    missing file from last commit

---
+++ NineMensMorris/src/GameLogic/RandomIAPlayer.java
@@ -0,0 +1,30 @@
+package GameLogic;
+
+public class RandomIAPlayer extends IAPlayer {
+
+	public RandomIAPlayer(int playerId) {
+		super(playerId);
+	}
+
+	@Override
+	public int getIndexToPlacePiece(Board gameBoard) {
+		while(true) {
+			int index = rand.nextInt(24);
+			if(!gameBoard.boardPositions[index].isOccupied) {
+				return index;
+			}
+		}
+	}
+
+	@Override
+	public int getIndexToRemovePieceOfOpponent(Board gameBoard) {
+		while(true) {
+			int index = rand.nextInt(24);
+			int playerOccupying = gameBoard.boardPositions[index].playerOccupying;
+			if(playerOccupying != Position.NO_PLAYER && playerOccupying != this.playerId) {
+				return index;
+			}
+		}
+	}
+
+}
