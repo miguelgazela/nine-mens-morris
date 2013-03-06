1613ff00ea2c389106fb65720de16aa31fd0e268
Acaldas <ei10051@I00CN1106.salas.feupsig.fe.up.pt>
2013-03-06 11:56:25 +0000

    minimax class

---
+++ NineMensMorris/src/GameLogic/MinimaxIAPlayer.java
@@ -0,0 +1,29 @@
+package GameLogic;
+
+public class MinimaxIAPlayer extends IAPlayer {
+
+	public MinimaxIAPlayer(int playerId,int depth) {
+		super(playerId);
+		
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getIndexToPlacePiece(Board gameBoard) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getIndexToRemovePieceOfOpponent(Board gameBoard) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Move getPieceMove(Board gameBoard) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
