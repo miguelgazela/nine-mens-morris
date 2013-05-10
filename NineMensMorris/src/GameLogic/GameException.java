b615d72e10e70362bf7e5ec692d73e34b380b29e
Miguel Oliveira <miguel.gazela@gmail.com>
2013-05-10 23:59:25 +0100

    structural changes

--- NineMensMorris/src/GameLogic/GameException.java
+++
@@ -1,9 +0,0 @@
-package GameLogic;
-
-public class GameException extends Exception{
-	private static final long serialVersionUID = -4036588148354448335L;
-
-	public GameException(String string) {
-		super(string);
-	}
-}
--- NineMensMorris/src/GameLogic/Token.java
+++
@@ -1,5 +0,0 @@
-package GameLogic;
-
-public enum Token {
-	PLAYER_1, PLAYER_2, NO_PLAYER;
-}
